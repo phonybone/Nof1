33c2ee0a3103360b83469d8a53e842ac78e6363a
Victor Cassen <phonybone@gmail.com>
2013-08-20 12:51:20 -0700

    adding Pipeline.py

---
+++ lib/Pipeline/Pipeline.py
@@ -0,0 +1,16 @@
+from .exceptions import *
+
+class Pipeline(object):
+    def __init__(self, name, host, working_dir, dry_run):
+        self.name=name
+        self.host=host
+        self.working_dir=working_dir
+        self.dry_run=dry_run
+
+    def __repr__(self):
+        return 'Pipeline %s: working_dir=%s' % (
+            self.name, 
+            self.working_dir)
+
+    def run(self):
+        raise AbstractMethodNotImplementedException('Pipeline.Pipeline.run')
